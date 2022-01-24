5ea8e2d7eb0df9fdcb8c1795363a9989f6551fa9
piptouque <piptouque@protonmail.com>
2022-01-24 01:05:42 +0100

    [+]: unconstrained system.

--- source/synth_uk.py
+++ source/synth_uk.py
@@ -3,9 +3,10 @@ import copy
 import numpy as np
 import numpy.typing as npt
 import scipy.integrate as integrate
+from itertools import chain
 
 from typing import Callable, Union, Tuple, List
-from abc import abstractmethod
+from abc import abstractmethod, abstractproperty
 
 AFloat = Union[float, npt.NDArray[float]]
 AInt = Union[int, npt.NDArray[int]]
@@ -14,8 +15,6 @@ AVector = Union[Tuple[float, float], npt.NDArray[Tuple[float, float]]]
 
 ACallableFloat = Union[Callable[[AFloat], AFloat],
                        npt.NDArray[Callable[[AFloat], AFloat]]]
-ACallableVector = Union[Callable[[AVector], AVector],
-                        npt.NDArray[Callable[[AVector], AVector]]]
 
 
 class GuitarStringData:
@@ -99,7 +98,7 @@ class ModalStructure:
         Returns:
             float: [description]
         """
-        raise NotImplementedError()
+        raise NotImplemented
 
     @abstractmethod
     def ksi_n(self, n: AInt) -> AFloat:
@@ -126,60 +125,7 @@ class ModalStructure:
         Returns:
             float: [description]
         """
-        raise NotImplementedError()
-
-
-class GuitarBody(ModalStructure):
-    def __init__(self, data: GuitarBodyData) -> None:
-        self.data = data
-
-    def f_n(self, n: AInt) -> AFloat:
-        return self.data.f_n[self.n[n]]
-
-    def ksi_n(self, n: AInt) -> AFloat:
-        return self.data.ksi_n[self.n[n]]
-
-    def m_n(self, n: AInt) -> AFloat:
-        return self.data.m_n[self.n[n]]
-
-
-class GuitarString(ModalStructure):
-    def __init__(self,  data: GuitarStringData) -> None:
-        self.data = data
-
-    def _p_n(self, n: AInt) -> AFloat:
-        """Some modal factor
-
-        Args:
-            n (int): [description]
-
-        Returns:
-            float: [description]
-        """
-        return (2*n + 1) * np.pi / (2 * self.data.l)
-
-    def f_n(self, n: AInt) -> AFloat:
-        p_n = self._p_n(n)
-        return self.data.c_t / (2 * np.pi) * p_n * (1 + p_n ** 2 * self.data.b / (2 * self.data.t))
-
-    def m_n(self, n: AInt) -> AFloat:
-        phi_n = self.phi_n(n)
-        if np.ndim(n) != 0:
-            integs = np.empty(n.shape, dtype=float)
-            for i in range(len(n)):
-                integ, err = integrate.quad(
-                    lambda x: phi_n[i](x)**2, 0, self.data.l)
-                integs[i] = integ
-            return self.data.rho * integs
-        else:
-            integ, err = integrate.quad(lambda x:  phi_n(x)**2, 0, self.data.l)
-            return self.data.rho * integ
-
-    def ksi_n(self, n: AInt) -> AFloat:
-        p_n = self._p_n(n)
-        f_n = self.f_n(n)
-        return (self.data.t * (self.data.eta_f + self.data.eta_a / (2 * np.pi * f_n))
-                + self.data.eta_b * self.data.b * p_n ** 2) / (2 * (self.data.t + self.data.b * p_n**2))
+        raise NotImplemented
 
     def c_n(self, n: AInt) -> AFloat:
         """Modal 'resistance'
@@ -203,6 +149,7 @@ class GuitarString(ModalStructure):
         """
         return (4 * np.pi**2) * self.m_n(n) * self.f_n(n) ** 2
 
+    @abstractmethod
     def phi_n(self, n: AInt) -> ACallableFloat:
         """Modeshapes
 
@@ -212,6 +159,111 @@ class GuitarString(ModalStructure):
         Returns:
             Callable[[npt.NDArray[float]], npt.NDarray[float]]: The modeshape function for mode n
         """
+        return NotImplemented
+
+    @abstractproperty
+    def extends(self) -> Tuple[float, float]:
+        """Extends of the structure along the vibrating dimension
+
+        Returns:
+            Tuple[float, float]: [description]
+        """
+        return NotImplemented
+
+    def ext_force_n(self, ext_force: Callable[[float, float], float], n: AInt) -> ACallableFloat:
+        phi_n = self.phi_n(n)
+        x_1, x_2 = self.extends
+
+        def _f(t):
+            if np.ndim(n) != 0:
+                ext_f = np.empty(n.shape, dtype=float)
+                for j in range(len(n)):
+                    def _g(x, t):
+                        f = ext_force(x, t)
+                        phi = phi_n[j](x)
+                        return f * phi
+                    integ, err = integrate.quad(_g, x_1, x_2, args=(t))
+                    ext_f[j] = integ
+                return ext_f
+            else:
+                ext_f, err = integrate.quad(lambda x, t: ext_force(
+                    x, t) * phi_n(x), x_1, x_2, args=(t))
+                return ext_f
+        return _f
+
+    def solve_unconstrained(self, q_n: AFloat, dq_n: AFloat, n: AInt, ext_force: Callable[[float, float], float], t: float) -> AFloat:
+        ext_force_n = self.ext_force_n(ext_force, n)
+        ext_force_n_t = ext_force_n(t)
+        c_n = self.c_n(n)
+        k_n = self.k_n(n)
+        m_n = self.m_n(n)
+        ddq_u_n = - (c_n * dq_n - k_n * q_n + ext_force_n_t) / m_n
+        return ddq_u_n
+
+
+class GuitarBody(ModalStructure):
+    def __init__(self, data: GuitarBodyData) -> None:
+        self.data = data
+
+    def f_n(self, n: AInt) -> AFloat:
+        return self.data.f_n[self.data.n[n]]
+
+    def ksi_n(self, n: AInt) -> AFloat:
+        return self.data.ksi_n[self.data.n[n]]
+
+    def m_n(self, n: AInt) -> AFloat:
+        return self.data.m_n[self.data.n[n]]
+
+    def phi_n(self, n: AInt) -> AFloat:
+        # no info on the modeshapes for the body.
+        return np.full(n.shape, lambda x: 0, dtype=type(Callable)) if np.ndim(n) != 0 else lambda x: 0
+
+    @ property
+    def extends(self) -> Tuple[float, float]:
+        # same thing, we don't know.
+        return (0, 0)
+
+
+class GuitarString(ModalStructure):
+    def __init__(self,  data: GuitarStringData) -> None:
+        self.data = data
+
+    def _p_n(self, n: AInt) -> AFloat:
+        """Some modal factor
+
+        Args:
+            n (int): [description]
+
+        Returns:
+            float: [description]
+        """
+        return (2*n + 1) * np.pi / (2 * self.data.l)
+
+    def f_n(self, n: AInt) -> AFloat:
+        p_n = self._p_n(n)
+        return self.data.c_t / (2 * np.pi) * p_n * (1 + p_n ** 2 * self.data.b / (2 * self.data.t))
+
+    def m_n(self, n: AInt) -> AFloat:
+        phi_n = self.phi_n(n)
+        x_1, x_2 = self.extends
+        if np.ndim(n) != 0:
+            integs = np.empty(n.shape, dtype=float)
+            for i in range(len(n)):
+                integ, err = integrate.quad(
+                    lambda x: phi_n[i](x)**2, x_1, x_2)
+                integs[i] = integ
+            return self.data.rho * integs
+        else:
+            integ, err = integrate.quad(lambda x:  phi_n(x)**2, 0, self.data.l)
+            return self.data.rho * integ
+
+    def ksi_n(self, n: AInt) -> AFloat:
+        p_n = self._p_n(n)
+        f_n = self.f_n(n)
+        return (self.data.t * (self.data.eta_f + self.data.eta_a / (2 * np.pi * f_n))
+                + self.data.eta_b * self.data.b * p_n ** 2) / (2 * (self.data.t + self.data.b * p_n**2))
+
+    def phi_n(self, n: AInt) -> ACallableFloat:
         p_n = self._p_n(n)
         if np.ndim(n) != 0:
             phi_ns = np.empty(n.shape, dtype=type(Callable))
@@ -227,15 +279,74 @@ class GuitarString(ModalStructure):
         else:
             return lambda x: np.sin(p_n * x)
 
+    @ property
+    def extends(self) -> Tuple[float, float]:
+        return (0, self.data.l)
+
 
 class CouplingSolver:
-    def __init__(self, structs: List[ModalStructure]) -> None:
+    def __init__(self, structs: List[ModalStructure], ext_forces: List[Callable[[float, float], float]]) -> None:
         self.structs = structs
+        self.ext_forces = ext_forces
 
-    def force_n(self, n: AInt) -> ACallableVector:
-        # no f_ext for now
-        c_ns = self.c_n(n)
-        k_ns = self.k_n(n)
+    def solve_constraints(a_ns: List[List[npt.NDArray[float]]], b_ns: List[npt.NDArray[float]]) -> List[List[npt.NDArray[float]]]:
+        """
+        For now, a_ns, b_ns are constants in time.
+        a_ns[i] are the list of constraints applied on sub-structure i.
+        b_ns[i] is the result of the constraints ?
+
+        Args:
+            a_ns (List[npt.NDArray[float]]): [description]
+            b_ns (List[npt.NDArray[float]]): [description]
+            n (AInt): [description]
+        """
+        assert len(a_ns) == len(self.structs) and \
+            len(b_ns) == len(self.structs)
+        #
+        a_mat = np.array(a_ns)
+        b_vec = np.array(b_ns)
+
+        m_halfinv_mat = np.diag(chain.from_iterable(
+            [np.pow(struct.m_n(n), -0.5) for struct in self.structs]))
+        #
+        b_mat = a_mat @ m_halfinv_mat
+        b_plus_mat = np.linalg.pinv(b_mat)
+        w_mat = 1 - m_halfinv_mat * b_plus_mat * a_mat
+        return None
+
+    def simulation(self, q_n_is: List[AFloat], dq_n_is: List[AFloat], n: AInt, nb_steps: int, h: float) -> Tuple[List[npt.NDArray[AFloat]]]:
+        def make_vec():
+            if np.ndim(n) != 0:
+                return [np.zeros(n.shape + (nb_steps,), dtype=float)
+                        for i in range(len(self.structs))]
+            else:
+                return [np.zeros((nb_steps,), dtype=float) for i in range(len(self.structs))]
+        q_ns = make_vec()
+        dq_ns = make_vec()
+        ddq_ns = make_vec()
+        ddq_u_ns = make_vec()
+        dq_half_ns = make_vec()
+        for i in range(len(self.structs)):
+            q_ns[i][..., 0] = q_n_is[i]
+            dq_ns[i][..., 0] = dq_n_is[i]
+        #
+        t_ks = np.arange(nb_steps) * h
+        for k in range(1, nb_steps):
+            for i in range(len(self.structs)):
+                struct = self.structs[i]
+                q_ns[i][..., k] = q_ns[i][..., k-1] + h * dq_ns[i][..., k-1] + \
+                    0.5 * h**2 * ddq_ns[i][..., k-1]
+                dq_half_ns[i][..., k] = dq_ns[i][..., k-1] + \
+                    0.5 * h * ddq_ns[i][..., k-1]
+                ddq_u_ns[i][..., k] = struct.solve_unconstrained(
+                    q_ns[i][..., k], dq_half_ns[i][..., k], n, self.ext_forces[i], t_ks[k])
+                # solve constraints
+                # TODO
+                ddq_ns[i][..., k] = ddq_u_ns[i][..., k]
+                #
+                dq_ns[i][..., k] = dq_ns[i][..., k-1] + 0.5 * \
+                    h * (ddq_ns[i][..., k-1] + ddq_ns[i][..., k])
+        return t_ks, q_ns, dq_ns, ddq_ns
 
 
 if __name__ == "__main__":
@@ -254,10 +365,35 @@ if __name__ == "__main__":
         string_data = json.load(
             config, object_hook=lambda d: GuitarStringData(**d))
     body_frame = pd.read_csv(args.body)
-    body_data = body_frame.to_numpy()
-    b = GuitarBody(**body_data)
+    body_frame = body_frame.to_dict()
+    for (k, v) in body_frame.items():
+        body_frame[k] = np.fromiter(v.values(), dtype=float)
+    body_data = GuitarBodyData(**body_frame)
+    b = GuitarBody(body_data)
     s = GuitarString(string_data)
 
+    x_f = 0.33
+    delta_x = 0.01
+    delta_t = 0.1
+
+    def ext_force_string(x, t):
+        return 1 if np.abs(x - x_f) <= delta_x/2 and t < delta_t else 0
+    solver = CouplingSolver([s, b], [ext_force_string, lambda x, t: 0])
+
+    n = np.arange(5)
+    nb_steps = 20
+    h = 0.01
+
+    q_n_is = [np.zeros(n.shape, dtype=float) for i in range(2)]
+    dq_n_is = [np.zeros(n.shape, dtype=float) for i in range(2)]
+
+    t_ks, q_ns, dq_ns, ddq_ns = solver.simulation(
+        q_n_is, dq_n_is, n, nb_steps, h)
+
+    print(q_ns[0])
+
+
+def test():
     n = np.arange(10)
     x = np.linspace(0, s.data.l, 50)
     v = np.empty((len(n), len(x)))
