6c47d536fe05328d4bc3f0c9e971bd1d43ad0947
piptouque <piptouque@protonmail.com>
2022-02-08 05:27:10 +0100

    [+]: Optimisated ESTER.

--- source/hr/util.py
+++ source/hr/util.py
@@ -3,7 +3,7 @@ import scipy.signal as sig
 import numpy as np
 import numpy.typing as npt
 
-from typing import Tuple
+from typing import Tuple, List
 
 # by Robert Bristow-Johnson
 # taken from: https://www.firstpr.com.au/dsp/pink-noise/
@@ -11,6 +11,42 @@ _PINK_FILTRE_COEFFS = {
     "b": [0.98443604, 0.83392334, 0.07568359],
     "a": [0.99572754, 0.94790649, 0.53567505],
 }
+
+
+def esprit_correlation_mat(x: npt.NDArray[complex], n: int) -> npt.NDArray[complex]:
+    """Gets correlation matrix used in ESPRIT algorithm
+
+    Args:
+        x (npt.NDArray[complex]): [description]
+        n (int): [description]
+
+    Returns:
+        npt.NDArray[complex]: [description]
+    """
+    x_h = scipy.linalg.hankel(x[:n], r=x[n - 1 :])
+    l = x.shape[0] - n + 1
+    return x_h @ x_h.transpose().conj() / l
+
+
+def esprit_ws(
+    x: npt.NDArray[complex], n: int, k: int
+) -> Tuple[npt.NDArray[complex], npt.NDArray[float]]:
+    """Gets W and W_per used in ESPRIT algorithm
+
+    Args:
+        x (npt.NDArray[complex]): [description]
+        n (int): [description]
+        k (int): [description]
+
+    Returns:
+        Tuple[npt.NDArray[complex], npt.NDArray[float]]: (n,k) and (n,n-k) W and W_per matrices
+    """
+    r_xx = esprit_correlation_mat(x, n)
+    u_1, _, _ = scipy.linalg.svd(r_xx)
+    # sigma = scipy.linalg.diagsvd(s, r_xx.shape[0], r_xx.shape[1])
+    w = u_1[:, :k]
+    w_per = u_1[:, k:]
+    return w, w_per
 
 
 def esprit(
@@ -27,14 +63,7 @@ def esprit(
     Returns:
         Tuple[npt.NDArray[float], npt.NDArray[float]]: (estimated normalised frequencies, estimated normalised dampings)
     """
-    n_s = len(x)  # signal's length
-    l = n_s - n + 1  # number of columns of the Hankel matrix
-    # Not needed for the 'hankel' function but used in the formula of R_XX
-    x_hankel = scipy.linalg.hankel(x[:n], x[n - 1 :])  # Hankel matrix
-
-    r_xx = x_hankel @ x_hankel.conj().T / l  # correlation matrix
-    u_1, _, _ = scipy.linalg.svd(r_xx)
-    w = u_1[:, :k]  # signal space matrix
+    w, _ = esprit_ws(x, n, k)
 
     w_down = w[:-1]
     w_up = w[1:]
@@ -191,7 +220,7 @@ def noise_filtre_coeffs(
     # smoothing_order: at least two times the length of the PSD's principal lobe (can be done visually)
     # AR_order: ~ 10
     # note: no need to give it the correct sample frequency,
-    # since it is only used to return the fftfreqs.
+    # since it is only used to return the fftfreqs.
     _, x_psd = sig.welch(x, fs=1, nfft=n_fft)
 
     # Step 2: estimating the noise's PSD with a median filtre (smoothing the signal's PSD)
@@ -234,3 +263,113 @@ def whiten(
     # denominator coeff of the FIR's transfer function is 1
     x_whitened = sig.lfilter(b, [1], x)
     return x_whitened
+
+
+def ester_error(
+    x: npt.NDArray[complex], n: int, p_max: int
+) -> List[npt.NDArray[complex]]:
+    """[summary]
+
+    Still don't know if p_max whether chosen or estimated,
+    and how.
+    Don't discard the bogus p=0 case,
+    because then the best order is the argmax!
+
+    Args:
+        x (npt.NDArray[complex]): [description]
+        n (int): [description]
+        p_max (int): Maximum order to consider, in [|1, n-2|]
+
+    Returns:
+        List[npt.NDArray[complex]]: Estimation error of p for p in [1, p_max]
+    """
+    # x: input signal
+    # n: n: number of lines in the Hankel matrix S
+    # K: ?
+    assert (
+        1 <= p_max < n - 1
+    ), f"Maximum order p_max={p_max} should be less than n-1={n-1}."
+
+    w, _ = esprit_ws(x, n, p_max)
+    w_cap = [w[:, :p] for p in range(p_max + 1)]
+    #
+    # nus are (p)-vecs
+    nu = [w_cap[p][-1].T.conj() for p in range(p_max + 1)]
+    #
+    psi_l = [None] * (p_max + 1)
+    psi_r = [None] * (p_max + 1)
+    psi_lr = [None] * (p_max + 1)
+    ksi_cap = [np.zeros((n - 1, p), dtype=w_cap[0].dtype) for p in range(p_max + 1)]
+    phi = [np.zeros(p, dtype=w_cap[0].dtype) for p in range(p_max + 1)]
+    e = [np.zeros((p, p), dtype=w_cap[0].dtype) for p in range(p_max + 1)]
+    for p in range(1, p_max + 1):
+        # Recursive computation of e[p]
+        # see Badeau et al. (2006) Table 1 for details on the algorithm
+        w_cap_down_pm = w_cap[p - 1][:-1]
+        w_cap_up_pm = w_cap[p - 1][1:]
+        w_down_p = w_cap[p][:-1, p - 1]
+        w_up_p = w_cap[p][1:, p - 1]
+        # print(w_down_p.shape, w_up_p.shape)
+        # 1. Update of the auxiliary matrix psi_mat[p]
+        psi_r[p] = w_cap_down_pm.T.conj() @ w_up_p
+        psi_l[p] = w_cap_up_pm.T.conj() @ w_down_p
+        psi_lr[p] = w_down_p.T.conj() @ w_up_p
+        # print(w_cap_up_pm.shape, w_down_p.shape)
+        # 2. Update of the auxiliary matrix ksi_mat[p]
+        # print(w_up_p.shape)
+        # print(w_cap_down_pm.shape, psi_r[p].shape)
+        # print(w_down_p.shape, psi_lr[p].shape)
+        ksi_p = w_up_p - w_cap_down_pm @ psi_r[p] - w_down_p * psi_lr[p]
+        # print(ksi_cap[p - 1].shape)
+        # print(ksi_cap[p][:, :-1].shape)
+        psi_l_p = psi_l[p]
+        ksi_cap[p][:, :-1] = ksi_cap[p - 1] - np.outer(w_down_p, psi_l_p.conj())
+        ksi_cap[p][:, -1] = ksi_p
+        # 3. Computation of e[p] from ksi_cap[p]
+        mu_p = nu[p][-1]
+        phi[p][:-1] = phi[p - 1] + mu_p * psi_l[p]
+        phi[p][-1] = psi_r[p].T.conj() @ nu[p - 1] + mu_p * psi_lr[p].conj()
+        w_cap_down_p = w_cap[p][:-1]
+        a = w_cap_down_p @ nu[p]
+        e[p] = ksi_cap[p] - 1 / (1 - np.linalg.norm(nu[p], ord=2) ** 2) * np.outer(
+            (w_cap_down_p @ nu[p]), phi[p].T.conj()
+        )
+    # discard p=0 bogus case.
+    e = e[1:]
+    return e
+
+
+def ester_inverse_error_func(
+    x: npt.NDArray[complex], n: int, p_max: int
+) -> npt.NDArray[float]:
+    """Gets the inverse error function J in the ESTER algorithm
+
+    Args:
+        x (npt.NDArray[complex]): [description]
+        n (int): [description]
+        p_max (int): [description]
+
+    Returns:
+        npt.NDArray[float]: [description]
+    """
+    e = ester_error(x, n, p_max)
+    j = np.array([1 / np.linalg.norm(e[p], ord=2) ** 2 for p in range(len(e))])
+    return j
+
+
+def ester(x: npt.NDArray[complex], n: int, p_max: int) -> npt.NDArray[float]:
+    """Gets the estimated ESM model order r using the ESTER algorithm.
+    see: http://ieeexplore.ieee.org/document/1576975/
+
+    Args:
+        x (npt.NDArray[complex]): [description]
+        n (int): [description]
+        p_max (int): [description]
+
+    Returns:
+        npt.NDArray[float]: [description]
+    """
+    j = ester_inverse_error_func(x, n, p_max)
+    # first index actually corresponds to p=1
+    r = np.argmax(j) + 1
+    return r
