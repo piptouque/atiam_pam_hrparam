d07cb8813470b16232fa596c4d64eb15cf0619e8
piptouque <piptouque@protonmail.com>
2022-02-10 17:37:10 +0100

    [+]: updated measure script.

--- source/expr/measures/Acquisition_FRF.m
+++ source/expr/measures/Acquisition_FRF.m
@@ -5,14 +5,14 @@
 close all
 
 %% Parametres
-fs = 51200; %Fr√©quence d'√©chantillonnage
+fs = 51200; %FrÈquence d'Èchantillonnage
 tps = 5; %Temps d'acquisition
 Sensibilite_Marteau = 23.61e-3; %Unite A MODIF
 Sensibilite_Accelero = 10.17e-3; %V/m.s^-2Unite A MODIF
-Sensibilite_Micro = 67.8e-3; % V/Pa √†
-% √† faire: sensibilit√© micro
+Sensibilite_Micro = 67.8e-3; % V/Pa ‡
+% ‡ faire: sensibilitÈ micro
 
-%% Acquisition Carte NI (apr√®s lancement de Init)
+%% Acquisition Carte NI (aprËs lancement de Init)
 s.Rate = fs;
 s.DurationInSeconds= tps;
 [data,time] = s.startForeground();
@@ -31,18 +31,18 @@ signal_micro = data(:, 3)/Sensibilite_Micro;
 largeur = 80; %nbre de point
 [signal_marteau_fen,Indice_deb,Fenetre_m_libre_1] = FenetragePorte(signal_marteau,largeur);
 
-%Fenetrage exponentiel de la r√©ponse de l'acc√©l√©rom√®tre  
+%Fenetrage exponentiel de la rÈponse de l'accÈlÈromËtre  
 Taux = 1e-4;
 %deb_libre_1 = find(signal_marteau == max(signal_marteau));
 [signal_accelero_fen,Fenetre_libre_acc_1] = FenetrageExponentiel(signal_accelero, Taux, Indice_deb);
 
 %Fenetrage porte du signal du microphone
 largeur = 80; %nbre de point
-[signal_micro_fen,Indice_micro_deb,Fenetre_micro_libre_1] = FenetragePorte(signal_micro,largeur);
+[signal_micro_fen,Fenetre_micro_libre_1] = FenetrageExponentiel(signal_micro,Taux, Indice_deb);
 
 
 
-%% Calcul de la FRF et de la coh√©rence
+%% Calcul de la FRF et de la cohÈrence
 Nfft = 2^nextpow2(length(signal_accelero));
 freq = [0:fs/Nfft:fs/2-1/Nfft];
 
@@ -57,7 +57,7 @@ FRF = CalculFRF_H1(F_fenetre,A_fenetre);
 scrsz = get(groot,'ScreenSize');
 figure('Position',[scrsz(3)/4 1 scrsz(3)/2 scrsz(4)])
 
-axTemp1 = subplot(4,2,1); %Signaux temporels du marteau (non-fenetr√©s et fenetr√©s)
+axTemp1 = subplot(4,2,1); %Signaux temporels du marteau (non-fenetrÈs et fenetrÈs)
 plot(time,signal_marteau); hold on
 plot(time,signal_marteau_fen, 'r'),
 plot(time,Fenetre_m_libre_1*max(signal_marteau),'r')
@@ -65,36 +65,36 @@ title('marteau')
 xlabel('temps [s]')
 ylabel('Force [N]')
 
-axTemp2 = subplot(4,2,3); %Signaux fr√©quentiels de l'acc√©l√©rom√®tre(non-fenetr√©s et fenetr√©s)
+axTemp2 = subplot(4,2,3); %Signaux frÈquentiels de l'accÈlÈromËtre(non-fenetrÈs et fenetrÈs)
 plot(time,signal_accelero), hold on
 plot(time,signal_accelero_fen, 'r')
 plot(time,Fenetre_libre_acc_1*max(signal_accelero),'r')
 title('accelerometre')
 xlabel('temps [s]')
-ylabel('Acc√©l√©ration [m.s^{-2}]')
+ylabel('AccÈlÈration [m.s^{-1}]')
 
-axFreq1 = subplot(4,2,2); %Signaux fr√©quentiels du marteau (non-fenetr√©s et fenetr√©s)
+axFreq1 = subplot(4,2,2); %Signaux frÈquentiels du marteau (non-fenetrÈs et fenetrÈs)
 plot(freq,db(F(1:Nfft/2))), hold on
 plot(freq,db(F_fenetre(1:Nfft/2)), 'r')
 title('marteau')
-xlabel('Fr√©quence [Hz]')
+xlabel('frequence [s]')
 ylabel('Force [N]')
 
-axFreq2 = subplot(4,2,4); %Signaux fr√©quentiels de l'acc√©l√©rom√®tre(non-fenetr√©s et fenetr√©s)
+axFreq2 = subplot(4,2,4); %Signaux temporels de l'accÈlÈromËtre(non-fenetrÈs et fenetrÈs)
 plot(freq,db(A(1:Nfft/2))), hold on
 plot(freq,db(A_fenetre(1:Nfft/2)), 'r')
 title('accelerometre')
-xlabel('Fr√©quence (Hz)')
-ylabel('Acc√©l√©ration [m.s^{-2}]')
+xlabel('frÈquence (Hz)')
+ylabel('AccÈlÈration [m.s^{-2}]')
 
 axFreq3 = subplot(4,1,3); %Module de la FRF
 plot(freq,db(FRF(1:Nfft/2)))
-xlabel('Fr√©quence (Hz)')
-ylabel('Acc√©l√©rance [dB]')
+xlabel('frÈquence (Hz)')
+ylabel('AccÈlÈrance [dB]')
 
 axFreq4 = subplot(4,1,4);  %Phase de la FRF
 plot(freq,unwrap(angle(FRF(1:Nfft/2))))
-xlabel('fr√©quence (Hz)')
+xlabel('frÈquence (Hz)')
 ylabel('Phase [deg]')
 
 linkaxes([axFreq1,axFreq2,axFreq3,axFreq4],'x')
@@ -111,10 +111,9 @@ Final.FRF = FRF(1:Nfft/2);
 Final.freq = freq;
 Final.time = time;
 Final.fs = fs;
-Final.fen.start = Indice_deb
-Final.fen.length = largeur
-
-file_name = 'FirstStringGuitar_CopperWired01mm_FifthFret_1.mat';
-Final.excitation_type = 'wire'
+Final.fen.start = Indice_deb;
+Final.fen.length = largeur;
+file_name = 'FirstStringTwelfthFret_Finger_Bridge_TensionMod_3.mat';
+Final.excitation_type = 'Finger';
 
 save(file_name, 'Final');
